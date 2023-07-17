e2ecc6915fa12792d219bdcd2b980749697ccfd7
Andrew Kobiz <andrewkobiz@gmail.com>
2023-07-17 20:49:53 +0300

    uploaded task 3

---
+++ Lection1/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first and then second strings.");
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+            string result;
+
+            if (firstString.Length > secondString.Length)
+            {
+                result = firstString + " " + secondString;
+            }
+            else if (firstString.Length == secondString.Length)
+            {
+                result = "None";
+            }
+            else
+            {
+                char splitter = firstString.First();
+                if (secondString.Contains(splitter))
+                {
+                    result = secondString.Substring(0, secondString.IndexOf(splitter));
+                }
+                else
+                {
+                    result = "Longest string does not have first symbol from shortest string.";
+                }
+            }
+
+            Console.WriteLine("Your result is: {0}", result);
+
+        }
+    }
+}
