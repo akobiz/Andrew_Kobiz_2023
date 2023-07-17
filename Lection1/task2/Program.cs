5ca2afd40dae553ab1b9ae1071760e27894dd894
Andrew Kobiz <andrewkobiz@gmail.com>
2023-07-17 19:41:04 +0300

    added task 2

---
+++ Lection1/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            Console.WriteLine("Enter 10 numbers: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter {0} number: ", i + 1);
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Enter number to dublicate it: ");
+            int numberToDublicate = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numberToDublicate == numbers[i])
+                {
+                    numbers.Insert(i, numberToDublicate);
+                    i += 1;
+                }
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write($"{numbers[i]}, ");
+            }
+        }
+    }
+}
